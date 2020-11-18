4120476322354e00344a4b81914e2a4583fa7128
Asel <asel.ergeshova2001@gmail.com>
2020-11-18 21:17:18 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        double[] array = new double[]{2.3, -2.5, 0.3, 3.1, -4.2, 0.9, -5.3, 4.9, 0.8, -8.3, 9.2, -0.1, 6.9, -0.5, 0.9};
+
+        int colichestvo = 0;
+        double sume = 0;
+
+        boolean vyklichatel = false;
+
+
+
+        for (int i = 0; i <array.length ; i++) {
+            if (array[i]< 0) {
+                vyklichatel = true;
+            }
+
+            if (array [i] > 0&& vyklichatel){
+                colichestvo++;
+                sume += array[i];
+            }
+
+        }
+        System.out.println(sume / colichestvo);
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
